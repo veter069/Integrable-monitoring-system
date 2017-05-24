27b4a71e31f6d8f5baf106503d22015e48445c21
veter069 <wind069@gmail.com>
2017-05-24 22:52:13 +0500

    It's kinda unstable

--- IMS_ESP8266_12E_ArduinoIDE/IMS_ESP8266_12E_ArduinoIDE.ino
+++ IMS_ESP8266_12E_ArduinoIDE/IMS_ESP8266_12E_ArduinoIDE.ino
@@ -1,6 +1,8 @@
-
 #include <ESP8266WiFi.h>
 #include "DHT.h"
+
+int LedPin = D0;
+int Shock = 4; //define the vibration sensor interface
 
 const char* ssid = "Frosters";
 const char* password = "!#DidYouTryHard?!A";
@@ -11,9 +13,9 @@ WiFiServer server(10050);
 
 #define DHTTYPE DHT22
 int DHT22_PIN = D2; 
-float temperature = 23;
-float humidity = 37;
-DHT dht(DHT22_PIN, DHTTYPE);
+float temperature = 666;
+float humidity = 666;
+DHT* dht;
 
 int WATER_PIN = D3;
 bool flooding = false;
@@ -25,9 +27,9 @@ int CO2_PIN = A0;
 int co2 = 0;
 
 void setup() {
-  Serial.begin(115200);
+  //Serial.begin(9600);
   delay(10);
-
+  pinMode(LedPin, OUTPUT);
   // prepare DHT22 pin
   pinMode(DHT22_PIN, OUTPUT);
   digitalWrite(DHT22_PIN, 0);
@@ -40,29 +42,31 @@ void setup() {
 
   // prepare CO2 Sensor pin
   pinMode(CO2_PIN, INPUT);
-  
+
+  dht = new DHT(DHT22_PIN, DHTTYPE);
   
   // Connect to WiFi network
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
+  //Serial.println();
+  //Serial.println();
+  //Serial.print("Connecting to ");
+  //Serial.println(ssid);
   
   WiFi.begin(ssid, password);
   
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
+    BlinkHard();
+    //Serial.println(".");
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
-  
+//  Serial.println("");
+//  Serial.println("WiFi connected");
+  BlinkHard();
   // Start the server
   server.begin();
-  Serial.println("Server started");
+  //Serial.println("Server started");
 
   // Print the IP address
-  Serial.println(WiFi.localIP());
+  //Serial.println(WiFi.localIP());
 }
 
 void loop() {
@@ -73,8 +77,8 @@ void loop() {
   }
   
   // Wait until the client sends some data
-  Serial.println("");
-  Serial.println("new client");
+  //Serial.println("");
+  //Serial.println("new client");
   while(!client.available()){
     delay(1);
   }
@@ -82,47 +86,52 @@ void loop() {
   
   // Read the first line of the request
   String req = client.readStringUntil('\r');
-  Serial.println(req);
+  //Serial.println(req);
 //  for(int i=0; i< req.length(); i++){
 //    Serial.println((int)(req[i]));  
 //  }
 
   if (req.indexOf("agent.ping") != -1){
     client.println("1");
-    Serial.println();
+    //Serial.println();
   } 
+  else if (req.indexOf("agent.hostname") != -1){
+    UpdateSensors();
+    client.println("ESP8266_ESP12E_NodeMCUv0.1");
+    //Serial.println(co2);
+  }
   else if (req.indexOf("agent.version") != -1){
     client.println("EnviMon 0.0.2");
-    Serial.println("EnviMon 0.0.2");
+    //Serial.println("EnviMon 0.0.2");
   } 
   else if (req.indexOf("environment.temperature") != -1){
     UpdateDHT();
     client.println(temperature);
-    Serial.println(temperature);
+    //Serial.println(temperature);
   } 
   else if (req.indexOf("environment.humidity") != -1){
     UpdateDHT();
     client.println(humidity);
-    Serial.println(humidity);
+    //Serial.println(humidity);
   } 
   else if (req.indexOf("environment.flooding") != -1){
     UpdateSensors();
     client.println(flooding);
-    Serial.println(flooding);
+    //Serial.println(flooding);
   }
   else if (req.indexOf("environment.motion") != -1){
     UpdateSensors();
     client.println(motion);
-    Serial.println(motion);
+    //Serial.println(motion);
   }
   else if (req.indexOf("environment.co2") != -1){
     UpdateSensors();
     client.println(co2);
-    Serial.println(co2);
+    //Serial.println(co2);
   }
   else {
     server.println("ZBXDZBX_NOTSUPPORTED");
-    Serial.println("ZBXDZBX_NOTSUPPORTED");
+    //Serial.println("ZBXDZBX_NOTSUPPORTED");
   }
   
   client.flush();
@@ -132,8 +141,9 @@ void loop() {
 }
 
 void UpdateDHT(){
-  temperature = dht.readTemperature();
-  humidity = dht.readHumidity();
+  temperature = dht->readTemperature();
+  humidity = dht->readHumidity();
+  delay(1000);
 }
 
 void UpdateSensors(){
@@ -145,5 +155,21 @@ void UpdateSensors(){
   motion = (digitalRead(MOTION_PIN) == HIGH);
 
   co2 = analogRead(CO2_PIN);
+  
+}
+
+void BlinkHard(){
+  digitalWrite(LedPin, LOW);
+  delay(500);
+  digitalWrite(LedPin, HIGH);
+  delay(500);
+  digitalWrite(LedPin, LOW);
+  delay(500);
+  digitalWrite(LedPin, HIGH);
+  delay(500);
+  digitalWrite(LedPin, LOW);
+  delay(500);
+  digitalWrite(LedPin, HIGH);
+  delay(500);
 }
 
